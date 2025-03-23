d4e46453aa4129a2714968d554d32652fe1609ac
Mike Monaco <mamonaco1973@gmail.com>
2025-03-23 16:39:32 -0400

    fix new rg name

--- 03-aks/main.tf
+++ 03-aks/main.tf
@@ -18,5 +18,5 @@ data "azurerm_resource_group" "aks_flaskapp_rg" {
 
 data "azurerm_container_registry" "flask_acr" {
   name                = "flaskapp${substr(data.azurerm_client_config.current.subscription_id, 0, 6)}"
-  resource_group_name = data.azurerm_resource_group.flask_container_rg.name
+  resource_group_name = data.azurerm_resource_group.aks_flaskapp_rg.name
 }
