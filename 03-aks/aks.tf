51fea172bea797a3d42307743a97a01b62a147a3
Mike Monaco <mamonaco1973@gmail.com>
2025-03-24 12:25:02 -0400

    first pass at aks.tf

---
+++ 03-aks/aks.tf
@@ -0,0 +1,27 @@
+resource "azurerm_kubernetes_cluster" "flask_aks" {
+  name                = "flask-aks"
+  location            = data.azurerm_resource_group.location
+  resource_group_name = data.azurerm_resource_group.name
+  dns_prefix          = "flaskkube"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.k8s_identity.id]
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
