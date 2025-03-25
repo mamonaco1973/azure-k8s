e9a9431c66f97bcfaa2f1dbc02a53928dd927c90
mamonaco1973 <mamonaco1973@gmail.com>
2025-03-25 20:05:39 +0000

    fix yaml

--- validate.sh
+++ validate.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+MAX_RETRIES=5
+RETRY_DELAY=30
+ATTEMPT=1
 
-# Parse the service object to get the service IP address
+while [ $ATTEMPT -le $MAX_RETRIES ]; do
+#  echo "Attempt $ATTEMPT to retrieve service IP..."
+  SERVICE_IP=$(kubectl get service flask-app-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-SERVICE_IP=$(kubectl get service flask-app-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  if [ -n "$SERVICE_IP" ]; then
+    echo "NOTE: Service IP retrieved: $SERVICE_IP"
+    break
+  fi
 
+  echo "WARNING: Failed to retrieve service IP. Retrying in $RETRY_DELAY seconds..."
+  sleep $RETRY_DELAY
+  ((ATTEMPT++))
+done
 
-# Check if the IP was successfully extracted
+# Final check after retries
 if [ -z "$SERVICE_IP" ]; then
-  echo "ERROR: Failed to retrieve the service IP address."
+  echo "ERROR: Failed to retrieve the service IP address after $MAX_RETRIES attempts."
   exit 1
 fi
 
-echo "NOTE: Ingress Load Balancer IP: $SERVICE_IP"
 
 while true; do
     HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST "http://$SERVICE_IP/candidate/John%20Smith")
@@ -30,7 +41,7 @@ done
 cd ./02-docker
 SERVICE_URL="http://$SERVICE_IP"
 echo "NOTE: Testing the AKS Solution."
-echo "NOTE: URL for AKS Deployent is $SERVICE_URL/gtg?details=true"
+echo "NOTE: URL for AKS Deployment is $SERVICE_URL/gtg?details=true"
 ./test_candidates.py "$SERVICE_URL"
 
 cd ..
