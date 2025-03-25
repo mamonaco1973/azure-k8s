c3b80e8eb3e2b639b2438f9275aa3b6816a6381a
Mike Monaco <mamonaco1973@gmail.com>
2025-03-25 10:01:17 -0400

    add nsg for load balancer

--- 01-acr/networking.tf
+++ 01-acr/networking.tf
@@ -11,3 +11,26 @@ resource "azurerm_subnet" "aks_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.240.0.0/16"]
 }
+
+resource "azurerm_network_security_group" "flask_lb_nsg" {
+  name                = "flask_lb_nsg"                                   # Name of the NSG
+  location            = azurerm_resource_group.aks_flaskapp_rg.location  # Azure region
+  resource_group_name = azurerm_resource_group.aks_flaskapp_rg.name      # Resource group for the NSG
+
+  security_rule {
+    name                       = "Allow-HTTP"                         # Rule name: Allow HTTP traffic
+    priority                   = 1002                                 # Rule priority
+    direction                  = "Inbound"                            # Traffic direction
+    access                     = "Allow"                              # Allow or deny rule
+    protocol                   = "Tcp"                                # Protocol type
+    source_port_range          = "*"                                  # Source port range
+    destination_port_range     = "80"                                 # Destination port
+    source_address_prefix      = "*"                                  # Source address range
+    destination_address_prefix = "*"                                  # Destination address range
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "flask-lb-nsg-assoc" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.flask_lb_nsg.id
+}
